af46bef712958c87b246e58282990574ff567668
TheViking816 <adrianlujan94@yahoo.es>
2026-01-20 13:10:17 +0100

    Fix base paths for Vercel and Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isVercel = process.env.VERCEL === '1';
+    const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
     return {
-      base: mode === 'production' ? '/MiTurno/' : '/',
+      base: isVercel ? '/' : (isGithubPages ? '/MiTurno/' : '/'),
       server: {
         port: 3000,
         host: '0.0.0.0',
